38fafe44c333f3384286dbe2173ba289d7084de1
michele <michelefraccaroli@gmail.com>
2018-12-22 11:06:22 +0100

    Errore di incompatibilità delìgli import delle librerie risolto

--- Clust.py
+++ Clust.py
@@ -2,10 +2,10 @@ from time import time
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from ensorflow.python.keras.models import Sequential
+from ensorflow.python.keras.layers import Conv2D, MaxPooling2D
+from ensorflow.python.keras.layers import Activation, Dropout, Flatten, Dense
+from ensorflow.python.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
 class bcolors:
     HEADER = '\033[95m'
